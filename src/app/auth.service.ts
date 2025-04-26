b3d82ea667568842af2db6b8d34885472564c87e
ANASTAD2000 <anasbaclibr@gmai.com>
2025-04-26 22:39:52 +0100

    add login() in  auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.get<any[]>(`${this.apiUrl}?email=${email}&password=${password}`);
+  }
 }
